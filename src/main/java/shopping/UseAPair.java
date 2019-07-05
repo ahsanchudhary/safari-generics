53eb827a1b7c9195aa442df2d1f95e5f37327fb7
Simon Roberts <simon@dancingcloudservices.com>
2019-07-05 09:09:49 -0600

    Mid Break

---
+++ src/main/java/shopping/UseAPair.java
@@ -0,0 +1,12 @@
+package shopping;
+
+import java.time.LocalDate;
+
+public class UseAPair {
+  public static void main(String[] args) {
+//    Pair<String> p = new Pair<>("String", LocalDate.now());
+    Pair<String> p = new Pair<>("Fred", "Jones");
+//    p.setLeft(LocalDate.now());
+    p.setLeft("Frederick");
+  }
+}
